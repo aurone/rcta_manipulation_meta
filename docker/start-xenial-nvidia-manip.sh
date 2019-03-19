edb6b837a9ff7dec82d9c49392cdb0e1b13246a0
Andrew Dornbush <andrew.dornbush@gmail.com>
2019-03-19 17:28:35 -0400

    add argument to container start script to set appropriate ros env variables

--- docker/start-xenial-nvidia-manip.sh
+++ docker/start-xenial-nvidia-manip.sh
@@ -4,6 +4,14 @@ DOCKERID=aurone
 CONTAINER=rcta-xenial-nvidia-manip
 TAG=1.0
 
+WITH_ROBOT=0
+while [ "$#" -ne "0" ]; do
+    case $1 in
+    "--robot") WITH_ROBOT=1;;
+    esac
+    shift
+done
+
 #XAUTH=/tmp/.docker.xauth
 #if [ ! -f $XAUTH ]; then
 #    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
@@ -47,17 +55,35 @@ TAG=1.0
 #    -v "/etc/shadow:/etc/shadow:ro" \
 #    -v "/etc/sudoers.d:/etc/sudoers.d:ro" \
 
-#    --env="ROS_MASTER_URI=http://tl1-1-am1:11311" \
-nvidia-docker container run \
-    --rm \
-    -it \
-    --privileged \
-    --network host \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="DISPLAY=$DISPLAY" \
-    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    -v "$RCTA_ROOT/rcta_ws:/home/rcta/rcta_ws:rw" \
-    -h $(hostname) \
-    --name $CONTAINER \
-    $DOCKERID/$CONTAINER:$TAG \
-    bash
+if [ "$WITH_ROBOT" -eq 1 ]; then
+    ROS_IP="$(echo -e `ip address | grep 172.16 | cut -f1 -d'/' | sed -e 's/^[[:space:]]*//' | cut -f2 -d' '`)"
+    nvidia-docker container run \
+        --rm \
+        -it \
+        --privileged \
+        --network host \
+        --env="QT_X11_NO_MITSHM=1" \
+        --env="ROS_IP=$ROS_IP" \
+        --env="ROS_MASTER_URI=http://tl1-1-am1:11311" \
+        --env="DISPLAY=$DISPLAY" \
+        -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -v "$RCTA_ROOT/rcta_ws:/home/rcta/rcta_ws:rw" \
+        -h $(hostname) \
+        --name $CONTAINER \
+        $DOCKERID/$CONTAINER:$TAG \
+        bash
+else
+    nvidia-docker container run \
+        --rm \
+        -it \
+        --privileged \
+        --network host \
+        --env="QT_X11_NO_MITSHM=1" \
+        --env="DISPLAY=$DISPLAY" \
+        -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -v "$RCTA_ROOT/rcta_ws:/home/rcta/rcta_ws:rw" \
+        -h $(hostname) \
+        --name $CONTAINER \
+        $DOCKERID/$CONTAINER:$TAG \
+        bash
+fi
